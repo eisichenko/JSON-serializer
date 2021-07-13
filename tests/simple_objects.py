fc00eed618d84dd27aa1a16b1cd63748afd6090b
Egor Isichenko <eisichenko@mail.ru>
2021-07-14 00:14:38 +0300

    added more tests

--- tests/simple_objects.py
+++ tests/simple_objects.py
@@ -2,4 +2,5 @@ sample_dicts = [{ 1: 'a', 'asd': { True: 123, None: [ {1:'a'}, 2, False] }},
                 {'a': {'b': {1: 2}}}
                 ]
 
-sample_lists = []
+sample_lists = [[1, 2, 'qwe', None, True, False, [1, {'a': {1: True}}]],
+                [[1, [2, 3], [4]]]]
--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,8 +1,30 @@
 import pytest
 from .simple_objects import *
 from json_serializer import *
+import os
+
 
 @pytest.mark.parametrize('obj', sample_dicts)
 def test_dictionaries(obj):
     new_obj = loads(dumps(obj))
+    assert new_obj == obj
+
+
+@pytest.mark.parametrize('obj', sample_lists)
+def test_lists(obj):
+    new_obj = loads(dumps(obj))
+    assert new_obj == obj
+
+    
+@pytest.fixture()
+def file_fixture():
+    with open('tests/temp_file_for_tests.txt', 'w+') as fp:
+        yield fp
+    os.remove('tests/temp_file_for_tests.txt')
+
+    
+@pytest.mark.parametrize('obj', sample_dicts)
+def test_files(file_fixture, obj):
+    dump(obj, file_fixture)
+    new_obj = load(file_fixture)
     assert new_obj == obj
